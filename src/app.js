01ada82b8d7fae8e3e0d58cd05258737b531e8fa
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2023-02-14 03:14:26 +0500

    Next e commerce site vyorstka

---
+++ src/app.js
@@ -0,0 +1,18 @@
+
+const app = Vue.createApp({
+    data() {
+        return {
+            card_info: infos,
+            brendsName: brendsName,
+            shops: shops,
+        }
+    },
+
+    methods: {},
+
+    mounted() {
+       
+    }
+})
+
+app.mount('#root')
---
+++ src/infos.js
@@ -0,0 +1,149 @@
+let infos = [
+    {
+        id: Date.now(),
+        title: "Apple Airpods Pro simsiz quloqchin, Oq rangda",
+        booking: "1230 ta buyurtma",
+        price: "1230 ta buyurtma"
+    }, 
+    {
+        id: Date.now(),
+        title: `Apple Macbook Pro 13" 2.4 2019 Intel core i5 DDR3 8 GB SSD 256 GB`,
+        booking: "230 ta buyurtma",
+        price: "16 114 285 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Apple watch 4 ",
+        booking: "543 ta buyurtma",
+        price: "4 234 000 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Robot - tozalagich Xiaomi Mi Robot",
+        booking: "876 ta buyurtma",
+        price: "1 340 000 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Oynali Fotoaparat Canon EOS 6D Body",
+        booking: "565 ta buyurtma",
+        price: "1 345 000 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Apple Airpods Pro simsiz quloqchin , Oq rangda",
+        booking: "1230 ta buyurtma",
+        price: "299 000 so'm",
+    },
+    {
+        id: Date.now(),
+        title: `Apple Macbook Pro 13" 2.4 2019 Intel core i5 DDR3 8 GB SSD 256 GB `,
+        booking: "230 ta buyurtma",
+        price: "16 114 285 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Apple watch 4",
+        booking: "543 ta buyurtma",
+        price: "4 234 000 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Robot - tozalagich Xiaomi Mi Robot",
+        booking: "876 ta buyurtma",
+        price: "1 340 000 so'm",
+    },
+    {
+        id: Date.now(),
+        title: "Oynali Fotoaparat Canon EOS 6D Body",
+        booking: "565 ta buyurtma",
+        price: "1 345 000 so'm",
+    },
+]
+
+let brendsName = [
+    {
+        url: "./images/hp.svg"
+    },
+    {
+        url: "./images/aser.svg"
+    },
+    {
+        url: "./images/asus.svg"
+    },
+    {
+        url: "./images/sumsung.svg"
+    },
+    {
+        url: "./images/apple.svg"
+    },
+    {
+        url: "./images/msi.svg"
+    },
+    {
+        url: "./images/mi.svg"
+    },
+    {
+        url: "./images/vivo.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+]
+
+let shops = [
+    {
+        url: "./images/elmakon.svg"
+    },
+    {
+        url: "./images/texnomart.svg"
+    },
+    {
+        url: "./images/mediapark.svg"
+    },
+    {
+        url: "./images/sandiq.svg"
+    },
+    {
+        url: "./images/goodzone.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+    {
+        url: "./images/shaxmat.svg"
+    },
+]
